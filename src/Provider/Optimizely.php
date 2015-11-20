6003ce68a615eeb75d0de7867b51f92f59f23699
Valentin Prugnaud <valentin.prugnaud@widerfunnel.com>
2015-11-19 17:28:21 -0800

    Initial commit

---
+++ src/Provider/Optimizely.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace WiderFunnel\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class Optimizely
+ * @package League\OAuth2\Client\Provider
+ */
+class Optimizely extends AbstractProvider
+{
+    use BearerAuthorizationTrait;
+
+    /**
+     * Domain
+     *
+     * @var string
+     */
+    public $domain = 'https://app.optimizely.com';
+
+    /**
+     * Api domain
+     *
+     * @var string
+     */
+    public $apiDomain = 'https://www.optimizelyapis.com/experiment/v1';
+
+    /**
+     * Get authorization url to begin OAuth flow
+     *
+     * @return string
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return $this->domain . '/oauth2/authorize';
+    }
+
+    /**
+     * Get access token url to retrieve token
+     *
+     * @param  array $params
+     *
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return $this->domain . '/oauth2/token';
+    }
+
+    /**
+     * Get provider url to fetch user details
+     *
+     * @param  AccessToken $token
+     *
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return $this->apiDomain . '/projects';
+    }
+
+    /**
+     * Get the default scopes used by this provider.
+     *
+     * This should not be a complete list of all scopes, but the minimum
+     * required for the provider user interface!
+     *
+     * @return array
+     */
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+
+    /**
+     * Check a provider response for errors.
+     *
+     * @link   https://developer.github.com/v3/#client-errors
+     * @throws IdentityProviderException
+     * @param  ResponseInterface $response
+     * @param  string $data Parsed response data
+     * @return void
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if ($response->getStatusCode() >= 400) {
+            throw new IdentityProviderException(
+                $data['message'] ?: $response->getReasonPhrase(),
+                $response->getStatusCode(),
+                $response
+            );
+        }
+    }
+
+    /**
+     * Generate a user object from a successful user details request.
+     *
+     * @param array $response
+     * @param AccessToken $token
+     * @return \League\OAuth2\Client\Provider\ResourceOwnerInterface
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        $user = new OptimizelyResourceOwner($response);
+
+        return $user->setDomain($this->domain);
+    }
+}
