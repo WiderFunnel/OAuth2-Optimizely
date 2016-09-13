1001e4da07e1b8548f7e55d3e174ee87a859c063
Valentin Prugnaud <valentin@whatdafox.com>
2016-09-13 06:51:03 -0700

    Create better example

---
+++ example/index.php
@@ -0,0 +1,63 @@
+<?php
+
+use Dotenv\Dotenv;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+session_start();
+
+$dotenv = new Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
+$provider = new WiderFunnel\OAuth2\Client\Provider\Optimizely([
+    'clientId' => getenv('OPTIMIZELY_CLIENT_ID'),
+    'clientSecret' => getenv('OPTIMIZELY_CLIENT_SECRET'),
+    'redirectUri' => getenv('OPTIMIZELY_CALLBACK_URL'),
+]);
+
+$token = $_SESSION['oauth2token'];
+
+if(!$token) {
+    if (!isset($_GET['code'])) {
+
+        // If we don't have an authorization code then get one
+        $authUrl = $provider->getAuthorizationUrl();
+        $_SESSION['oauth2state'] = $provider->getState();
+        include_once __DIR__ . '/templates/index.php';
+
+// Check given state against previously stored one to mitigate CSRF attack
+    } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+
+        unset($_SESSION['oauth2state']);
+        exit('Invalid state!');
+
+    } else {
+
+        // Try to get an access token (using the authorization code grant)
+        $token = $provider->getAccessToken('authorization_code', [
+            'code' => $_GET['code']
+        ]);
+
+        $_SESSION['oauth2token'] = $token;
+
+        // Optional: Now you have a token you can look up a users profile data
+        try {
+
+            // We got an access token, let's now get the projects details
+            $projects = $provider->getResourceOwner($token)->toArray();
+
+            include_once __DIR__ . '/templates/project.php';
+
+        } catch (Exception $e) {
+
+            // Failed to get user details
+            exit('Oh dear...');
+        }
+
+        // Use this to interact with an API on the users behalf
+        echo $token->getToken();
+    }
+} else {
+    $projects = $provider->getResourceOwner($token)->toArray();
+    include_once __DIR__ . '/templates/project.php';
+}
---
+++ example/templates/index.php
@@ -0,0 +1,5 @@
+<?php include_once __DIR__ . '/header.php';?>
+<a href="<?php echo $authUrl;?>">
+    <img src="img/optimizely.png" alt="Optimizely" width="200">
+</a>
+<?php include_once __DIR__ . '/footer.php';?>
