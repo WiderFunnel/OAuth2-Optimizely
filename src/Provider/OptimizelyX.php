b8e6cd0aacc8041e4034773bb75bfcf97273223c
Valentin Prugnaud <valentin.prugnaud@widerfunnel.com>
2017-08-25 12:32:47 -0700

    Add a class to support Optimizely X

---
+++ src/Provider/OptimizelyX.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GrowthOptimized\OAuth2\Client\Provider;
+
+/**
+ * Class OptimizelyX
+ * @package GrowthOptimized\OAuth2\Client\Provider
+ */
+class OptimizelyX extends Optimizely
+{
+    /**
+     * Api domain
+     *
+     * @var string
+     */
+    protected $apiDomain = 'https://api.optimizely.com/v2';
+}
